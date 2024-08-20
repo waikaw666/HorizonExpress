ab5ed48913602626cf8426ec005fd3c997b8bf4b
Zai <130903099+zaibutcooler@users.noreply.github.com>
2024-08-20 16:53:39 +0630

    configured postgres

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SearchResultController;
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 Route::get('/', function () {
     return view('home');
 });
@@ -12,19 +15,13 @@ Route::get('/', function () {
 Route::get('/main/contact', function () {
     return view('main.contact');
 });
+
 Route::get('/main/about', function () {
     return view('main.about');
 });
 
 
-Route::get('/test', function () {
-    $term1 = 'Mandalay';
-    $term2 = 'Yangon';
-    $data1 = \App\Models\Schedule::all();
-    $data1->bus->
-    dd($data1);
-});
-
+Route::get('/test', [TestController::class, 'index']);
 
 
 Route::get('/search', [SearchController::class, 'index']);
